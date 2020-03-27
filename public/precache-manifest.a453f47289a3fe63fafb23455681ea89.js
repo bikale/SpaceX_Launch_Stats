a597e2bb0bd076b3dd8dfd067ee92e7a2b0e456a
“bikale” <bikaleg@gmail.com>
2020-03-27 00:46:42 -0500

    deploy spacexstat

---
+++ public/precache-manifest.a453f47289a3fe63fafb23455681ea89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acc6cf20fdb94358ff155b0c112d213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb444267f28fd659cbed",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1081bcc99d6e3aeba233",
+    "url": "/static/js/2.59bf6197.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.59bf6197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb444267f28fd659cbed",
+    "url": "/static/js/main.720ff1ac.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
